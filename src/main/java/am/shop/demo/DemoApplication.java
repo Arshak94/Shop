2030c42576d0508c7703f7f597a11c78ef829787
Arayik <araik.armenia@gmail.com>
2018-12-22 01:41:48 +0400

    dzaca

--- src/main/java/am/shop/demo/DemoApplication.java
+++ src/main/java/am/shop/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("Arayik jan nayi sovori gyors chem tanelu :D");
+        System.out.println("Fsyo sax toshnia dzel em");
     }
 
 }
